e605a9453fd504d4fde9345b3cc04823333f32a2
Nishant <nishant203@gmail.com>
2020-11-30 19:33:54 +0530

    Initial commit

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>Filter</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register </router-link>
+    </div>
+    <ul>
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      debugger;
+      const data = this.$store.getters.coaches;
+      return data;
+    },
+  },
+};
+</script>
---
+++ src/pages/requests/ContactCoach.vue
@@ -0,0 +1,3 @@
+<template>
+    Contact Template
+</template>
